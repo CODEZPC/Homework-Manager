09fbe3234c415b3c90ad6b10c8ff2c13b60c707c
CODEZPC <zpcpy@outlook.com>
2026-06-24 12:12:31 +0800

    1.6.2.12 配置文件修复逻辑修复

--- src/main.py
+++ src/main.py
@@ -31,8 +31,8 @@ import updater
 COLOR = "#767F89"
 DEBUG = False
 DATA = "homework.json"
-VERSION = "1.6.2.11"
-VERSION_NUM = 1006002011
+VERSION = "1.6.2.12"
+VERSION_NUM = 1006002012
 tk = None
 
 
